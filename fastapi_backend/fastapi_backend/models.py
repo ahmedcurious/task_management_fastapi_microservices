7fd2fa71dda1d01bb64acac2f2f105f94441b698
ahmedcurious <ahmed_naseem1@outlook.com>
2024-06-10 18:10:15 +0500

    Second Commit: Added my Task Scehma Model in models.py file

--- fastapi_backend/fastapi_backend/models.py
+++ fastapi_backend/fastapi_backend/models.py
@@ -1,2 +1,10 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional, List
+
+class TaskBase(SQLModel):
+    title: str = Field(max_length=255)
+    description: Optional[str] = Field(default=None)
+    completed: bool = False
+
+class Task(TaskBase, table=True):
+    id: int = Field(default=None, primary_key=True, index=True)
