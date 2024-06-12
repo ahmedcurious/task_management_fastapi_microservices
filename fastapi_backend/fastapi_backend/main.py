fdff8af6e26abfc5b98d435bf26b8295ca617a27
ahmedcurious <ahmed_naseem1@outlook.com>
2024-06-13 00:09:33 +0500

    Fourth Commit: Three API endpoints are successfully working with the database

--- fastapi_backend/fastapi_backend/main.py
+++ fastapi_backend/fastapi_backend/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, HTTPException, Path, Query
+from fastapi import FastAPI, HTTPException, Path, Query, Depends
 from typing import List, Optional
-from uuid import UUID, uuid4
-from .models import Task
-from .db import init_db
 from contextlib import asynccontextmanager
+from sqlmodel import Session, select
+from .models import Task
+from .db import init_db, get_session
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,25 +14,33 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-# @app.post("/tasks/", response_model=Task)
-# def create_task(task: Task):
-#     task.id = uuid4()
-#     tasks.append(task)
-#     return task
+@app.post("/tasks/", response_model=Task)
+async def create_task(task_data: Task,
+                      session: Session = Depends(get_session)
+                      ):
+    task = Task(title=task_data.title,
+                description=task_data.description, completed=task_data.completed)
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
 
 
-# @app.get("/tasks/", response_model=List[Task])
-# def read_task():
-#     return tasks
+@app.get("/tasks/", response_model=List[Task])
+async def read_task(session: Session = Depends(get_session)):
+    task_list = session.exec(select(Task)).all()
+    return task_list
 
 
-# @app.get("/tasks/{task_id}", response_model=Task)
-# def read_individual_task(task_id: UUID):
-#     for task in tasks:
-#         if task.id == task_id:
-#             return task
+@app.get("/tasks/{task_id}", response_model=Task)
+async def read_individual_task(task_id: int,
+                               session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task Not Found")
+    return task
 
-#     raise HTTPException(status_code=404, detail="Task Not Found")
+
 
 # @app.put("/tasks/{task_id}", response_model=Task)
 # def update_individual_task(task_id: UUID, task_update: Task):
@@ -40,7 +49,7 @@ app = FastAPI(lifespan=lifespan)
 #             updated_task = task.copy(update=task_update.model_dump(exclude_unset=True))
 #             tasks[index] = updated_task
 #             return updated_task
-        
+
 #     raise HTTPException(status_code=404, detail="task not found")
 
 # @app.delete("/tasks/{task_id}", response_model=Task)
@@ -48,5 +57,5 @@ app = FastAPI(lifespan=lifespan)
 #     for index, task in enumerate(tasks):
 #         if task.id == task_id:
 #             return tasks.pop(index)
-        
+
 #     raise HTTPException(status_code=404, detail="task not found")
